063b8f5dbe1cab746124bab70696a05258c4ff6a
Hrubý Jakub (151643) <hrubyj@vutbr.cz>
2019-06-10 15:58:29 +0200

    Commit from server!

--- resources/views/common/contact.blade.php
+++ resources/views/common/contact.blade.php
@@ -3,7 +3,14 @@
 @section('content')
 <div class="container">
     <h2 class="my-3">Contact us</h2>
+
+    Developer <strong>Filip Janko</strong> <br>
     E-mail: <strong>fikoun@icloud.com</strong> <br>
-    Phone: <strong>+420 728 975 192</strong>
+    Phone: <strong>+420 728 975 192</strong><br>
+    <br>
+	Supervisor <strong>Jakub Hrubý</strong> <br>
+    E-mail: <strong>jakub.hruby@ceitec.vutbr.cz</strong> <br>
+    Phone: <strong>+420 608 072 211</strong>
 </div>
+
 @endsection
