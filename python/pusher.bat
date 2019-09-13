1ada8b00bd4321b115936c0e28daf25e086c7346
Hrubý Jakub (151643) <hrubyj@vutbr.cz>
2019-09-13 16:46:12 +0200

    Commit from server!

---
+++ python/pusher.bat
@@ -0,0 +1,3 @@
+python sql_pusher.py
+
+pause
--- python/sql_pusher.py
+++ python/sql_pusher.py
@@ -17,25 +17,18 @@ mydb = mysql.connector.connect(
 	database="data_processing"
 )
 	
-temp = temp.replace('\n','').replace('\r', '').replace('>','')
-temp = round(( int(temp) - 32 ) * 5/9, 1)
+temp = 7
 time = datetime.datetime.now().strftime(f)
 try:
 	db.execute(f"INSERT INTO `data` (`measurement_id`, `type`, `value`, `created_at`, `updated_at`) VALUES ('1', 'temp', '{temp}', '{time}', '{time}');")
 	mydb.commit()
-
 except mysql.connector.Error as err:
 	print(err)
 	print("Error Code:", err.errno)
 	print("SQLSTATE", err.sqlstate)
 	print("Message", err.msg)
-print("Teplota: ", round(temp,1))
-	
-	serial.close() 
 
 def interval(delay):
-	
-
 
 	time.sleep()
 	interval()
